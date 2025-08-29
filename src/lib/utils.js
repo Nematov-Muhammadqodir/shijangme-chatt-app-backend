d157ddb095b19b2a1b71dcd5c77b9c2db4b708a8
ya.nematov <nematovmuhammadqodir68@gmail.com>
2025-08-29 18:22:30 +0900

    fix: secure: false

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: true, // only true in prod with HTTPS
+    secure: false, // only true in prod with HTTPS
     sameSite: "none", // allow cross-origin
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
